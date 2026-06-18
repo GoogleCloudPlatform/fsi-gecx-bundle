a76936543e794e67e4233cfc0ea0f181494df1ab
Mark Servidio <mservidio@google.com>
2026-06-18 19:28:51 -0400

    removing mortgage preapproval

--- gecx/Mortgage_Preapproval/tools/escalate_to_agent/python_function/python_code.py
+++
@@ -1,9 +0,0 @@
-def escalate_to_agent() -> None:
-    context.variables["TELEPHONY_PAYLOAD"] = {
-      "ujet": {
-        "type": "action",
-        "action": "escalation",
-        "escalation_reason": "by_virtual_agent"
-      }
-    }
-    print("TELEPHONY_PAYLOAD" + str(context.variables["TELEPHONY_PAYLOAD"]))
